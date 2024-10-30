dd87fc82b45881e814e9e9d4c542d00deb6e5912
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-10-30 07:17:40 +0400

    (nix) poetry2nix attempt #1

--- flake.nix
+++ flake.nix
@@ -1,19 +1,44 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, poetry2nix }:
     let
       inherit (nixpkgs) lib;
       inherit (builtins) attrValues;
       eachSystem = f:
         lib.genAttrs [ "x86_64-linux" ]
         (system: f nixpkgs.legacyPackages.${system});
+      inherit (poetry2nix.lib.mkPoetry2Nix {
+        inherit (nixpkgs.legacyPackages."x86_64-linux")
+        ;
+      })
+        mkPoetryApplication;
     in {
+      packages.default = mkPoetryApplication {
+        projectDir = self;
+        overrides = poetry2nix.overrides.withDefaults (final: super:
+          lib.mapAttrs (attr: systems:
+            super.${attr}.overridePythonAttrs (old: {
+              nativeBuildInputs = (old.nativeBuildInputs or [ ])
+                ++ map (a: final.${a}) systems;
+            })) {
+              # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
+              # package = [ "setuptools" ];
+            });
+
+      };
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
           packages = attrValues { inherit (pkgs) python3 poetry; };
+          inputsFrom = [ self.packages.default ];
         };
       });
 
